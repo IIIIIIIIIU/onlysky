a0df5fcab8a160bcad3d01367b2ee36ce19c5ef6
IIIIIIIIIU <32150313+IIIIIIIIIU@users.noreply.github.com>
2017-09-21 15:03:57 +0800

    Create 1867.cpp

---
+++ 1867.cpp
@@ -0,0 +1,208 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <cassert>
+#include <algorithm>
+const int Big_B = 1000000000; const int Big_L = 9;
+using namespace std;
+inline int intcmp_ (int a, int b) { if (a > b) return 1; return a < b ? -1 : 0; }
+struct Int 
+{
+#define rg register
+    inline int max (int a, int b) { return a > b ? a : b; }
+    inline int min (int a, int b) { return a < b ? a : b; }
+    std :: vector <int> c; Int () {} typedef long long LL; 
+    Int (int x) { for (; x > 0; c.push_back (x % Big_B), x /= Big_B); }
+    Int (LL x) { for (; x > 0; c.push_back (x % Big_B), x /= Big_B); }
+    inline void CrZ () { for (; !c.empty () && c.back () == 0; c.pop_back ()); }
+    inline Int &operator += (const Int &rhs)
+    {
+        c.resize (max (c.size (), rhs.c.size ())); rg int i, t = 0, S;
+        for (i = 0, S = rhs.c.size (); i < S; ++ i)
+            c[i] += rhs.c[i] + t, t = c[i] >= Big_B, c[i] -= Big_B & (-t);
+        for (i = rhs.c.size (), S = c.size (); t && i < S; ++ i)
+            c[i] += t, t = c[i] >= Big_B, c[i] -= Big_B & (-t);
+        if (t) c.push_back (t); return *this;
+    }
+    inline Int &operator -= (const Int &rhs)
+    {
+        c.resize (max (c.size (), rhs.c.size ())); rg int i, t = 0, S;
+        for (i = 0, S = rhs.c.size (); i < S; ++ i)
+            c[i] -= rhs.c[i] + t, t = c[i] < 0, c[i] += Big_B & (-t);
+        for (i = rhs.c.size (), S = c.size (); t && i < S; ++ i)
+            c[i] -= t, t = c[i] < 0, c[i] += Big_B & (-t);
+        CrZ (); return *this;
+    }
+    inline Int &operator *= (const Int &rhs)
+    {
+        rg int na = c.size (), i, j, S, ai; 
+        c.resize (na + rhs.c.size ()); LL t;
+        for (i = na - 1; i >= 0; -- i)
+        {
+            ai = c[i], t = 0, c[i] = 0;
+            for (j = 0, S = rhs.c.size (); j < S; ++ j)
+            {
+                t += c[i + j] + (LL) ai * rhs.c[j];
+                c[i + j] = t % Big_B, t /= Big_B;
+            }
+            for (j = rhs.c.size (), S = c.size (); t != 0 && i + j < S; ++ j)
+                t += c[i + j], c[i + j] = t % Big_B, t /= Big_B;
+            assert (t == 0);
+        }
+        CrZ (); return *this;
+    }
+    inline Int &operator /= (const Int &rhs) { return *this = div (rhs); }
+    inline Int &operator %= (const Int &rhs) { return div (rhs), *this; }
+    inline Int &shlb (int l = 1)
+    {
+        if (c.empty ()) return *this; c.resize (c.size () + l);rg int i;
+        for (i = c.size () - 1; i >= l; -- i) c[i] = c[i - l];
+        for (i = 0; i < l; ++ i) c[i] = 0;
+        return *this;
+    }
+    inline Int &shrb (int l = 1)
+    {
+        for (rg int i = 0; i < c.size () - l; ++ i) c[i] = c[i + l];
+        c.resize (max (c.size () - l, 0)); return *this;
+    }
+    inline int Comp (const Int &rhs) const
+    {
+        if (c.size () != rhs.c.size ()) return intcmp_ (c.size (), rhs.c.size ());
+        for (rg int i = c.size () - 1; i >= 0; -- i) 
+            if (c[i] != rhs.c[i]) return intcmp_ (c[i], rhs.c[i]);
+        return 0;
+    }
+    inline Int div (const Int &rhs)
+    {
+        assert (!rhs.c.empty ()); Int q, r; rg int i; if (rhs > *this) return 0;
+        q.c.resize (c.size () - rhs.c.size () + 1); rg int _l, _r, mid;
+        for (i = c.size () - 1; i > c.size () - rhs.c.size (); -- i) r.shlb (), r += c[i];
+        for (i = c.size () - rhs.c.size (); i >= 0; -- i)
+        {
+            r.shlb (); r += c[i]; 
+            if (r.Comp (rhs) < 0) q.c[i] = 0;
+            else
+            {
+                _l = 0, _r = Big_B;
+                for (; _l != _r; )
+                {
+                    mid = _l + _r >> 1;
+                    if ((rhs * mid).Comp (r) <= 0) _l = mid + 1; else _r = mid;
+                }
+                q.c[i] = _l - 1, r -= rhs * q.c[i];
+            }
+        }
+        q.CrZ (), *this = r; return q;
+    }
+    friend inline Int operator + (const Int &lhs, const Int &rhs)
+    { Int res = lhs; return res += rhs; }
+    friend inline Int operator - (const Int &lhs, const Int &rhs)
+    { Int res = lhs; return res -= rhs; }   
+    friend inline Int operator * (const Int &lhs, const Int &rhs)
+    { Int res = lhs; return res *= rhs; }
+    friend inline Int operator / (const Int &lhs, const Int &rhs)
+    { Int res = lhs; return res.div (rhs); }
+    friend inline Int operator % (const Int &lhs, const Int &rhs)
+    { Int res = lhs; return res.div (rhs), res; }
+    friend inline ostream &operator << (ostream &out, const Int &rhs)
+    { 
+        if (rhs.c.size () == 0) out << "0";
+        else
+        {
+            out << rhs.c.back ();
+            for (rg int i = rhs.c.size () - 2; i >= 0; -- i)
+                out << setfill ('0') << setw (Big_L) << rhs.c[i];
+        }
+        return out;
+    } 
+    friend inline bool operator < (const Int &lhs, const Int &rhs)
+    { return lhs.Comp (rhs) < 0; }
+    friend inline bool operator <= (const Int &lhs, const Int &rhs)
+    { return lhs.Comp (rhs) <= 0; }
+    friend inline bool operator > (const Int &lhs, const Int &rhs)
+    { return lhs.Comp (rhs) > 0; }
+    friend inline bool operator >= (const Int &lhs, const Int &rhs)
+    { return lhs.Comp (rhs) >= 0; }
+    friend inline bool operator == (const Int &lhs, const Int &rhs)
+    { return lhs.Comp (rhs) == 0; }
+    friend inline bool operator != (const Int &lhs, const Int &rhs)
+    { return lhs.Comp (rhs) != 0; }
+#undef rg   
+};
+typedef Int ll;
+ll gcd(ll a,ll b)
+{
+    return b!=0?gcd(b,a%b):a;
+}
+struct node
+{
+    ll up,down;
+    node(ll up_=0,ll down_=0)
+    {
+        up=up_,down=down_;
+    }
+    node operator+(const node &pos)const
+    {
+        if(up==0&&pos.up==0) return node(0,0);
+        if(up==0||down==0) return node(pos.up,pos.down);
+        if(pos.up==0||pos.down==0) return node(up,down);
+        node res;
+        res.up=up*pos.down+pos.up*down;
+        res.down=down*pos.down;
+        ll tmp=gcd(res.up,res.down);
+        res.up/=tmp,res.down/=tmp;
+//      if(res.down<0)
+//      {
+//          ll daw=0;
+//          cout<<pos.up<<' '<<pos.down<<endl;
+//          cout<<up<<' '<<down<<endl;
+//      }
+        return res;
+    }
+    node operator*(const ll pos)const
+    {
+        node res(up,down*pos);
+        if(res.up==0) return res;
+        ll tmp=gcd(res.up,res.down);
+        res.up/=tmp;
+        res.down/=tmp;
+        return res;
+    }
+};
+struct node dp[60][60];
+ll n,m;
+int main()
+{
+    int x,y;
+    cin>>x>>y;n=x,m=y;
+    char op[5];
+    dp[1][1]=node(1,1);
+    for(int i=1;i<=x;i++)
+        for(int v=1;v<=i;v++)
+        {
+            cin>>op;
+            if(dp[i][v].up==0||dp[i][v].down==0) continue;
+            if(op[0]=='*')
+            {
+                dp[i+1][v]=dp[i+1][v]+(dp[i][v]*2);
+                dp[i+1][v+1]=dp[i+1][v+1]+(dp[i][v]*2);
+            }
+            else dp[i+2][v+1]=dp[i+2][v+1]+dp[i][v];
+        }
+    /*
+    for(ll i=1;i<=n+1;i++)
+    {
+        for(ll v=1;v<=i;v++)
+        {
+            printf("%d,%d ",dp[i][v].up,dp[i][v].down);
+        }
+        printf("\n");
+    }
+    */
+//  printf("%lld",dp[n+1][m+1].up);
+    cout<<dp[x+1][y+1].up;
+    putchar('/');
+    cout<<dp[x+1][y+1].down;
+    return 0;
+}
